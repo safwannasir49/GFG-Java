9981fbaacd7f16ee5c34b89fb93ab88b109dd7b2
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-06-19 22:00:09 +0530

    Create Print With Space.java

---
+++ Print With Space/Print With Space.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public void utility(Scanner scn) {
+        // code here
+        String a = scn.nextLine();
+        String b = scn.nextLine();
+        System.out.print(a+" "+b);
+        System.out.println();
+    }
+}
