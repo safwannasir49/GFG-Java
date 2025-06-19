bff40f67d4fffd0a38c9ea3e813f5800640ba262
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-06-19 22:02:27 +0530

    Create Swap The Numbers.java

---
+++ Swap The Numbers/Swap The Numbers.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    public void swap(int a, int b) {
+        // code here
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a + " " + b);
+    }
+}
