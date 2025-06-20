1c58162962c72ae0a3e72fb3acf8c7958f87d5e9
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-06-20 07:59:12 +0530

    Create The If Statement.java

---
+++ The If Statement/The If Statement.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+class Solution {
+    public static void utility(int n) {
+        // Write your if statement below
+            if(n>100) System.out.println("Big");
+        // Write your if statement above
+        System.out.println("Number");
+    }
+}
