43a66b08630086ee25c930aa09942286ba11e979
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-06-20 08:05:12 +0530

    Create Even Odd Game.java

---
+++ Even Odd Game/Even Odd Game.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+class Solution {
+    public static String utility(int n) {
+
+        // write your code here
+        return (n%2==0) ? "Friend" : "You";
+
+    }
+}
